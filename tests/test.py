5e85a3aa44132fd6bffaecb6d4f300dffe0d7b3b
evan <evan@evanw.org>
2021-12-27 21:17:32 -0600

    add support for referenced and short referenced images

--- tests/test.py
+++ tests/test.py
@@ -12,6 +12,8 @@ class Tests(unittest.TestCase):
             '<p><img alt="" src="foo.png" /></p>'
         )
 
+        # FIXME: dont want newline before closing figure tag
+
         # captioned img
         self.assertEqual(
             md.convert('![caption](foo.png)'),
@@ -22,6 +24,18 @@ class Tests(unittest.TestCase):
         self.assertEqual(
             md.convert('![caption](foo.png "title")'),
             '<p>\n<figure><img src="foo.png" title="title" /><figcaption>caption</figcaption>\n</figure>\n</p>'
+        )
+
+        # referenced image
+        self.assertEqual(
+            md.convert('![caption][ref]\n[ref]: foo.png'),
+            '<p>\n<figure><img src="foo.png" /><figcaption>caption</figcaption>\n</figure>\n</p>'
+        )
+
+        # short referenced image
+        self.assertEqual(
+            md.convert('![caption]\n[caption]: foo.png'),
+            '<p>\n<figure><img src="foo.png" /><figcaption>caption</figcaption>\n</figure>\n</p>'
         )
 
     def test_captions_attr(self):
@@ -46,6 +60,18 @@ class Tests(unittest.TestCase):
                 + '\n<figure class="class2"><img src="foo2.png" /><figcaption>caption2</figcaption></figure>\n</p>'
 
             )
+        )
+
+        # referenced image w/ attr_list
+        self.assertEqual(
+            md.convert('![caption][ref]{: .class}\n[ref]: foo.png'),
+            '<p>\n<figure class="class"><img src="foo.png" /><figcaption>caption</figcaption></figure>\n</p>'
+        )
+
+        # short referenced image w/ attr_list
+        self.assertEqual(
+            md.convert('![caption]{: .class}\n[caption]: foo.png'),
+            '<p>\n<figure class="class"><img src="foo.png" /><figcaption>caption</figcaption></figure>\n</p>'
         )
 
 
